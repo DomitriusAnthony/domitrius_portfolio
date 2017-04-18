20017b335d6dde926ec2623a9515fdc11b7ccafa
Domitrius Clark <domitrius.anthony@hotmail.com>
2017-04-18 04:38:01 -0400

    Finished projects display with angular tabs fully functional

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,4 @@
 //= require angular
 //= require turbolinks
 //= require_tree .
+
--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,12 +1,12 @@
 // # Place all the behaviors and hooks related to the matching controller here.
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
-(function() {
-  var app = angular.module('portfolioProject', []);
+(function(){
+  var app = angular.module('portfolio', []);
 
-  app.controller('ProjectController', function(){
-  	this.project = websites
-  })
+  app.controller('PortfolioController', function(){
+  	this.projects = websites;
+  });
 
 
   app.controller('TabController', function(){
@@ -21,10 +21,19 @@
     };
   });
 
-})
-
-var websites = [{
-	name: 'PressStart'
-	description: "PressStart is a social platform for gamers and content creators to connect and build their following. This app allows users to signup/login through Devise, upload images with Paperclip & AWS s3, make posts/comments, follow/unfollow other users, and chat in real time using Websockets/Action Cable and Redis."
-	image: "images/pressstart.jpg"
-}]
+	var websites = [{
+		name: 'PressStart',
+		description: "PressStart is a social platform for gamers and content creators to connect and build their following. This app allows users to signup/login through Devise, upload images with Paperclip & AWS s3, make posts/comments, follow/unfollow other users, and chat in real time using Websockets/Action Cable and Redis.",
+		image: "../assets/pressstart.png",
+		link1: "www.pressstart.herokuapp.com",
+		link2: "www.github.com/DomitriusAnthony/pressstart"
+		}, 
+	{
+		name: "School'd",
+		description: "School'd is targeted for middle school level kids to complete multiple challenges to learn the basic HTML & CSS setup for a page. By the end of the challenges they will have a fully fleshed out profile page, created by them. The challenges are JS DOM-manipulation which store in the database. ",
+		image: "../assets/schoold.png",
+		link1: "www.schoold.herokuapp.com",
+		link2: "www.github.com/DomitriusAnthony/schoold"
+	}
+	]
+})();
