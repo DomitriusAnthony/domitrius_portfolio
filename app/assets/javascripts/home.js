f26a7c3dbac709819dd937bb367f96f8a0ab4f15
Domitrius Clark <domitrius.anthony@hotmail.com>
2017-04-19 01:16:08 -0400

    Finishing some styling between the sections of the page and adding 'about' and 'social' paragraphs

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,7 +2,7 @@
 // # All this logic will automatically be available in application.js.
 // # You can use CoffeeScript in this file: http://coffeescript.org/
 (function(){
-  var app = angular.module('portfolio', []);
+  var app = angular.module('portfolio', [])
 
   app.controller('PortfolioController', function(){
   	this.projects = websites;
@@ -26,14 +26,16 @@
 		description: "PressStart is a social platform for gamers and content creators to connect and build their following. This app allows users to signup/login through Devise, upload images with Paperclip & AWS s3, make posts/comments, follow/unfollow other users, and chat in real time using Websockets/Action Cable and Redis.",
 		image: "../assets/pressstart.png",
 		link1: "www.pressstart.herokuapp.com",
-		link2: "www.github.com/DomitriusAnthony/pressstart"
+		link2: "www.github.com/DomitriusAnthony/pressstart",
+		recording: "../assets/pressstart-recording.mp4"
 		}, 
 	{
 		name: "School'd",
 		description: "School'd is targeted for middle school level kids to complete multiple challenges to learn the basic HTML & CSS setup for a page. By the end of the challenges they will have a fully fleshed out profile page, created by them. The challenges are JS DOM-manipulation which store in the database. ",
 		image: "../assets/schoold.png",
 		link1: "www.schoold.herokuapp.com",
-		link2: "www.github.com/DomitriusAnthony/schoold"
+		link2: "www.github.com/DomitriusAnthony/schoold",
+		recording: "../assets/schoold-recording.mp4"
 	}
 	]
 })();
